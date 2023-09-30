470d4a3e9f3ee833ef6651e053d2c775ab0952a4
Leonel Balsas <rldlab84@gmail.com>
2023-09-30 10:34:57 -0300

    making this live

--- app.js
+++ app.js
@@ -11,51 +11,85 @@ server.headersTimeout = 120 * 1000;
 
 const html = `
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El sonido del cobre</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-26M9SWDWV7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-26M9SWDWV7');
+</script>
+<body style="background-color: lightblue">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="/images/logos/photo_2023-09-21_18-38-55.jpg" alt="Logo">Cobre Sonoro</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Productos
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">AWG 42</a></li>
+                  <li><a class="dropdown-item" href="#">AWG 42.5</a></li>
+                  <li><a class="dropdown-item" href="#">AWG 43</a></li>
+                  <li><a class="dropdown-item" href="#">AWG 44</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Alambres de cobre esmaltados</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="html/aboutus.html">Asesoramiento</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="html/contact.html">Nosotros</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="html/aboutus.html">Contacto</a>
+              </li>
+            </ul>
+            <!--<form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          -->
+          </div>
+        </div>
+      </nav>
+    <h1>Cobre Sonoro.</h1>
+    <h2>Diámetros AWG 38 al 45 Importado, y 0,05 al 0,30 mm nacional.</h2>
+    <h2>Consultas: <a href="cobrelepy@gmail.com">cobrelepy@gmail.com</a></h2>
+    <h2>Whatsapp: 11 5047 6024</h2>        
+    <iframe width="420" height="315" src="https://www.youtube.com/embed/SEOA1pYgDOQ">
+    </iframe>
+    <br>
+    <img src="images/copper1.jpg">
+    <img src="/images/photo_2023-09-13_17-18-23.jpg" alt="">
+    <img src="/images/photo_2023-09-13_17-18-41.jpg" alt="">
+    <img src="/images/photo_2023-09-13_17-18-44.jpg" alt="">
+    <img src="/images/photo_2023-09-13_17-18-48.jpg" alt="">
+    <img src="/images/photo_2023-09-13_17-18-51.jpg" alt="">
+    <img src="/images/photo_2023-09-13_17-18-54.jpg" alt="">
+    <img src="/images/photo_2023-09-13_17-18-58.jpg" alt="">   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
 </html>
 `
