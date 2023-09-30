e532012770c78d7419b29a96d2abcc2bee3200e6
Leonel Balsas <rldlab84@gmail.com>
2023-09-30 10:57:25 -0300

    last

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ const html = `
     <img src="/images/photo_2023-09-13_17-18-48.jpg" alt="">
     <img src="/images/photo_2023-09-13_17-18-51.jpg" alt="">
     <img src="/images/photo_2023-09-13_17-18-54.jpg" alt="">
-    <img src="/images/photo_2023-09-13_17-18-58.jpg" alt="">   
+    <img src="/images/photo_2023-09-13_17-18-58.jpg" alt="">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
