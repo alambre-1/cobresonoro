78c99ed2216bfc71ea8f991cc3d9e5d78aaa8ea7
Leonel Balsas <rldlab84@gmail.com>
2023-09-30 14:02:37 -0300

    excellent

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-app.post("/send/", function (req, res) {
+app.post("/", function (req, res) {
+  console.log("nicely done dude")
   res.send(req.query)
 });
 
