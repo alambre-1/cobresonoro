985b09b4f495f803ccb8aabbf0001967e7abe09c
Leonel Balsas <rldlab84@gmail.com>
2023-10-09 15:32:17 -0300

    html

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.use('/images', express.static(__dirname + "/images"));
 //app.use(express.static('/images'))
 app.use('/css', express.static(__dirname + "/css"));
 app.use('/js', express.static(__dirname + "/js"));
+app.use('/html', express.static(__dirname + "/html"));
 
 /*app.get("/", (req, res) => res.type('html').send(html));*/
 app.use(express.static(__dirname + "/html")); 
